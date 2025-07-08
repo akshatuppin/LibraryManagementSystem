3abafab980c96969d8854d0d6e8ca9c7b97ee4ff
Akshat Uppin <akshat.uppin_it23@pccoer.in>
2025-07-08 08:59:29 +0530

    This is Initial files

---
+++ src/LibrarySystem/Main.java
@@ -0,0 +1,30 @@
+package LibrarySystem;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
+    private static final String root = "root";
+    private static final String password = "Akshat@1026" ;
+
+    public static void main(String args []){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try{
+            Connection connection = DriverManager.getConnection(url,root,password);
+            Scanner scanner = new Scanner(System.in);
+            User user = new User(connection,scanner);
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/LibrarySystem/User.java
@@ -0,0 +1,18 @@
+package LibrarySystem;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class User {
+    private Connection connection ;
+
+    private Scanner scanner ;
+
+    public User(Connection connection , Scanner scanner){
+        this.connection = connection ;
+        this.scanner = scanner ;
+    }
+
+
+
+}
